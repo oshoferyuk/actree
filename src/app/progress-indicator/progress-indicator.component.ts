522758d8914dd3e60a9831adee0771ad7776f237
Oleksiy Shoferyuk <o.shoferyuk@softerra.llc>
2019-07-11 18:51:52 +0300

    5 dots

--- src/app/progress-indicator/progress-indicator.component.ts
+++ src/app/progress-indicator/progress-indicator.component.ts
@@ -5,28 +5,19 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
   selector: 'app-progress-indicator',
   animations: [
     trigger(`signal`, [
-      state('state11', style({ backgroundColor: 'blue' })),
-      state('state12', style({ backgroundColor: 'blue' })),
-      state('state21', style({ backgroundColor: 'blue' })),
-      state('state22', style({ backgroundColor: 'blue' })),
-      state('state31', style({ backgroundColor: 'blue' })),
-      state('state32', style({ backgroundColor: 'blue' })),
-      state('state41', style({ backgroundColor: 'blue' })),
-      state('state42', style({ backgroundColor: 'blue' })),
-      state('state51', style({ backgroundColor: 'blue' })),
-      state('state52', style({ backgroundColor: 'blue' })),
+      state('state11', style({ })),
+      state('state12', style({  })),
 
-    transition('* => *', [
+    transition('state11 => state12', [
       style({position: 'absolute'}),
       animate('{{timing}}', keyframes([
       style({transform: 'translateX({{translateX0}}px)', offset: 0.0, animationTimingFunction: 'cubic-bezier(0.01,0.75,1,1)'}),
       style({transform: 'translateX({{translateX1}}px)', offset: 0.28, animationTimingFunction: 'linear'}),
       style({transform: 'translateX({{translateX2}}px)', offset: 0.66, animationTimingFunction: 'cubic-bezier(0,0,0.99,0.25)'}),
       style({transform: 'translateX({{translateX3}}px)', offset: 0.8, animationTimingFunction: 'linear'}),
-      style({transform: 'translateX({{translateX4}}px)', offset: 1.0, animationTimingFunction: 'linear'}),
+      style({transform: 'translateX({{translateX4}}px)', offset: 1.0, animationTimingFunction: 'linear'})
     ]))
       ], {params: {width2: 50, timing: '3s 1s'}})
-
     ]),
   ],
   templateUrl: './progress-indicator.component.html',
@@ -45,23 +36,18 @@ export class ProgressIndicatorComponent implements OnInit {
   ngOnInit() {
 
     this.state1 = this.getParameters('state11', '3s 0s');
-    this.state2 = this.getParameters('state21', '3s 0.2s');
-    this.state3 = this.getParameters('state31', '3s 0.4s');
-    this.state4 = this.getParameters('state41', '3s 0.6s');
-    this.state5 = this.getParameters('state51', '3s 0.8s');
-
-  }
-
-  stop(){
-
+    this.state2 = this.getParameters('state11', '3s 0.2s');
+    this.state3 = this.getParameters('state11', '3s 0.4s');
+    this.state4 = this.getParameters('state11', '3s 0.6s');
+    this.state5 = this.getParameters('state11', '3s 0.8s');
   }
 
   onDone($event: any){
     this.state1 = this.state1.value == 'state11' ? this.getParameters('state12', '3s 0s') : this.getParameters('state11', '3s 0s');
-    this.state2 = this.state2.value == 'state21' ? this.getParameters('state22', '3s 0.2s') : this.getParameters('state21', '3s 0.2s');
-    this.state3 = this.state3.value == 'state31' ? this.getParameters('state32', '3s 0.4s') : this.getParameters('state31', '3s 0.4s');
-    this.state4 = this.state4.value == 'state41' ? this.getParameters('state42', '3s 0.6s') : this.getParameters('state41', '3s 0.6s');
-    this.state5 = this.state5.value == 'state51' ? this.getParameters('state52', '3s 0.8s') : this.getParameters('state51', '3s 0.8s');
+    this.state2 = this.state2.value == 'state11' ? this.getParameters('state12', '3s 0.2s') : this.getParameters('state11', '3s 0.2s');
+    this.state3 = this.state3.value == 'state11' ? this.getParameters('state12', '3s 0.4s') : this.getParameters('state11', '3s 0.4s');
+    this.state4 = this.state4.value == 'state11' ? this.getParameters('state12', '3s 0.6s') : this.getParameters('state11', '3s 0.6s');
+    this.state5 = this.state5.value == 'state11' ? this.getParameters('state12', '3s 0.8s') : this.getParameters('state11', '3s 0.8s');
   }
 
   getParameters(name, delay){
